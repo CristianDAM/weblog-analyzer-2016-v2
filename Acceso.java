32c12e4872c3bb818d83ba1fc78ebf6ea40cd832
CristianDAM <gutierrezperezcristian@gmail.com>
2017-03-03 13:29:34 +0100

    Acceso con parametro String

--- Acceso.java
+++ Acceso.java
@@ -5,36 +5,38 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+
+    public Acceso(String parametrosAPasar)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        String[] parametrosConstructor = parametrosAPasar.split(" ");
+        ano = Integer.parseInt(parametrosConstructor[0]); 
+        mes = Integer.parseInt(parametrosConstructor[1]); 
+        dia = Integer.parseInt(parametrosConstructor[2]);
+        hora = Integer.parseInt(parametrosConstructor[3]); 
+        minutos = Integer.parseInt(parametrosConstructor[4]);               
+
     }
-    
+
     public int getAno() 
     {
         return ano;
     }
-    
+
     public int getMes()
     {
         return mes;
     }
-    
+
     public int getDia()
     {
         return dia;
     }
-    
+
     public int getHora()
     {
         return hora;
     }
-    
+
     public int getMinutos()
     {
         return minutos;
